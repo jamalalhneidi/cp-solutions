b84b4bc6b27472646759d70ccb4023c1df9bd80a
Jamal Alhneidi <jamal.alhneidi@gmail.com>
2024-01-06 00:00:23 +0100

    f

--- .github/actions/tag-parser/src/index.js
+++ .github/actions/tag-parser/src/index.js
@@ -4,5 +4,8 @@ import path from 'path';
 
 (async () => {
     const res = await cli.exec('git diff --name-only HEAD HEAD~1');
-    fs.writeFileSync(path.join(path.resolve(), 'test.json'), res);
+    fs.writeFileSync(
+        path.join(path.resolve(), 'test.json'),
+        JSON.stringify(res)
+    );
 })();
