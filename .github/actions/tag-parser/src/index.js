65403b52b128ee67e465ce956ce1894be12fc60a
Jamal Alhneidi <jamal.alhneidi@gmail.com>
2024-01-16 16:36:30 +0100

    fix

--- .github/actions/tag-parser/src/index.js
+++ .github/actions/tag-parser/src/index.js
@@ -3,6 +3,7 @@ import fs from 'fs';
 import path from 'path';
 
 (async () => {
+    console.log(path.resolve());
     console.log((await cli.getExecOutput('git branch')).stdout);
     const res = await cli.getExecOutput('git diff --name-only HEAD HEAD~1');
     console.log(`writing file to ${path.join(path.resolve(), 'test.txt')}`);
